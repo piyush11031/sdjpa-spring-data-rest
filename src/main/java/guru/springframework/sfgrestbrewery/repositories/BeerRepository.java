397669dabd6eca75812a51e31ffce288e93e6840
piyush11031 <pareekpiyush747@gmail.com>
2023-12-01 17:39:13 +0530

    Intro to Spring Data REST

--- src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
@@ -6,10 +6,13 @@ import guru.springframework.sfgrestbrewery.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
 
+@RepositoryRestResource(path = "beer", //The path segment under which this resource is to be exported
+                        collectionResourceRel = "beer") //The rel value to use when generating links to the collection resource.
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
--- src/test/java/guru/springframework/sfgrestbrewery/SfgRestfulBreweryApplicationTests.java
+++ src/test/java/guru/springframework/sfgrestbrewery/SfgRestfulBreweryApplicationTests.java
@@ -10,5 +10,4 @@ public class SfgRestfulBreweryApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
 }
